de21756d232568b5360767c700a4c3c2fffd295a
Wamaitha <wamaithaweru19@gmail.com>
2022-09-28 20:26:11 +0300

    create form class

---
+++ src/app/hero.ts
@@ -0,0 +1,12 @@
+export class Hero {
+    constructor(
+        public id:number,
+        public name:string,
+        public email:string,
+        public power:string,
+        public county:string,
+        public example:string,
+        public alterEgo?:string,
+    ){}
+
+}
