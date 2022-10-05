6313fee5826a416d711523c452fd5fe09f1c2585
Wamaitha <wamaithaweru19@gmail.com>
2022-10-05 11:20:18 +0300

    create component for Js Intermediate quiz questions

---
+++ src/app/js-intermediate/js-intermediate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-js-intermediate',
+  templateUrl: './js-intermediate.component.html',
+  styleUrls: ['./js-intermediate.component.css']
+})
+export class JsIntermediateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
