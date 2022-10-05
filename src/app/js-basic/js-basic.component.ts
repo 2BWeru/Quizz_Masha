cd779fdffa43ac6b17a560d5503f8dc3f4d401d9
Wamaitha <wamaithaweru19@gmail.com>
2022-10-05 11:19:58 +0300

    create component for Js Basic quiz questions

---
+++ src/app/js-basic/js-basic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-js-basic',
+  templateUrl: './js-basic.component.html',
+  styleUrls: ['./js-basic.component.css']
+})
+export class JsBasicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
