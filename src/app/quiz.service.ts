255c055dba7474f162fcce7804004cd98ce21665
Wamaitha <wamaithaweru19@gmail.com>
2022-10-08 12:28:47 +0300

    create getQuiz api function, create variable to store api key and url

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { Pipe, PipeTransform } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
+@Pipe({
+  name: 'objectToArray',
+})
 export class QuizService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  BaseHtmlUrl="https://quizapi.io/api/v1/questions?apiKey=UvJWWhRup0tmxi53AsB11ruaJogO2Zjjt968vlHr&limit=17&tags=HTML"
+
+  ApiKey="UvJWWhRup0tmxi53AsB11ruaJogO2Zjjt968vlHr"
+
+  getQuizQuestions(Object:any=[]){
+    return this.http.get<any>(this.BaseHtmlUrl);
+  };
 }
