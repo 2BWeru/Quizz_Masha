7bfb7c0d6f3e6df4e4d38df6335b5f44f3fdbd84
Wamaitha <wamaithaweru19@gmail.com>
2022-10-29 10:35:06 +0300

    edits landing page  style

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit,ElementRef, ViewChild, ViewChildren } from '@angular/core';
+import { Component, OnInit,ElementRef, ViewChild} from '@angular/core';
 import { Hero } from '../hero';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { RouterLink } from '@angular/router';
 import {  Router} from '@angular/router';
-
+import {NgForm} from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-blog',
@@ -15,6 +15,9 @@ export class BlogComponent implements OnInit {
   @ViewChild("name") nameKey!:ElementRef;
 
   public btnName :any;
+  public quizAttempts:number=1;
+  public attempts:number=0;
+
  
   constructor(private modalService: NgbModal,private router:Router) { }
 
@@ -50,6 +53,29 @@ export class BlogComponent implements OnInit {
    // submit form
   submitted=false
 
-  onSubmit(){this.submitted=true;}
+  // onSubmit(){this.submitted=true;}
+
+  saveName(event:any){
+    if(localStorage.getItem("emailPerson") !== null){
+      console.log(localStorage.getItem("emailPerson"));
+        this.router.navigate(['/blog']);
+        alert("You have already Registered")
+    }else{
+      console.log(" null");
+      this.router.navigate(['/names']);
+    }
+  }
+
+  quizPath(){
+
+  }
+
+
+  onSubmit(){
+    console.log("adding form values ");
+    // console.log(addMountForm.value);
+}
+
+
 
 }
