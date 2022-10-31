f12c6905e270d30f2a893a22e3fbf1f121271de0
Wamaitha <wamaithaweru19@gmail.com>
2022-10-31 17:43:47 +0300

    add time pipe and id generating funcion

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TotalService } from '../total.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-results',
@@ -7,22 +8,39 @@ import { TotalService } from '../total.service';
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent implements OnInit {
+  // define datepipe
+
+  datePipe: DatePipe = new DatePipe('en-US');
+
+ 
+
 
   constructor(private totalService:TotalService) { }
 
   score:any;
+  today:number = Date.now();
   test:string="";
+  username:any;
 
   ngOnInit(): void {
     this.totalService.currentData.subscribe(data => {
       this.score=data;
       console.log(this.score);
+
+      this.username=localStorage.getItem("namePerson");
    });
 
    this.totalService.currentType.subscribe(data => {
     this.test=data;
     console.log(this.test);
  });
+
+//  unique id
+
+   
 }
+
+
+
 
 }
